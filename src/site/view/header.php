28daff7b97284b444e2dcf770509aef12f56d36a
DoubleVKay <vovikhangcdv@gmail.com>
2020-06-11 17:52:46 +0700

    add signup, signin feature

--- src/site/view/header.php
+++ src/site/view/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-  <base href="/public/">
+  <base href="/public/spicyX/">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">    
@@ -39,7 +39,6 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-
   </head>
   <body>  
   <!-- Pre Loader -->
