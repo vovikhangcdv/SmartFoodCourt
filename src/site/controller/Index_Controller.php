a3f4d5406a50adf4ce976d6b76a5bc07c062c346
DoubleVKay <vovikhangcdv@gmail.com>
2020-06-16 10:37:56 +0700

    RESTART PROJECT: clean up unuseful files, update database file, resolve conficts.

--- src/site/controller/Index_Controller.php
+++ src/site/controller/Index_Controller.php
@@ -30,36 +30,4 @@ class Index_Controller extends Base_Controller {
         $this->view->load('another',$data);
         $this->view->load('footer',$data);
     }
-    public function menuAction(){
-                // $this->config->load('debug_config');
-        global $Database;
-        $this->model->load('Db');
-        $data = array('title' => 'BK Smart Food Court');
-        $list_vendor = get_all_by_tablename($Database,'vendor');
-        $number_vendor = count($list_vendor);
-        for ($i = 0; $i < $number_vendor; $i++) {
-            $data['list_vendor'][$list_vendor[$i]['id']]['categories'] = get_category_products_of_vendor($Database,$list_vendor[$i]['id']);
-            $data['list_vendor'][$list_vendor[$i]['id']]['products'] = get_products_of_vendor($Database,$list_vendor[$i]['id']);
-
-        }
-        if(isset($_GET['vendor_id'])) $data['vendor_id'] = intval($_GET['vendor_id']); 
-        else $data['vendor_id'] = 1;
-        $this->view->load('header',$data);
-        $this->view->load('slider',$data);
-        $this->view->load('menu',$data);
-        $this->view->load('footer',$data);
-    }
-
-    public function modify_menuAction(){
-        $this->view->load('header',$data);
-        $this->view->load('slider',$data);
-        $this->view->load('modify_menu',$data);
-        $this->view->load('footer',$data);
-    }
-    public function testAction(){
-        // $this->view->load('header',$data);
-        // $this->view->load('slider',$data);
-        $this->view->load('history_page',$data);
-        // $this->view->load('footer',$data);
-    }
 }
