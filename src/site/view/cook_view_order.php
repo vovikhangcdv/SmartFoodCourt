994b6c3ea332a74cff6664808ce0655844c2df31
cOrN_ak <khoa.ngobk@hcmut.edu.vn>
2020-06-24 18:49:53 +0700

    Update UI for menu.php and add cook_view_order.php

---
+++ src/site/view/cook_view_order.php
@@ -0,0 +1,92 @@
+<section id="mu-about-us">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="mu-about-us-area">
+                    <div class="mu-title">
+                        <span class="mu-subtitle">Cook</span>
+                        <h2>VIEW ORDER</h2>
+                        <i class="fa fa-spoon"></i>
+                        <span class="mu-title-bar"></span>
+                    </div>
+                    <aside style="padding-left: 180px;">
+                        <table style="width:80%; font-size:large;" class="j2store-cart-table table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th style="text-align:center; width:15%;">Order ID</th>
+                                    <th style="text-align:center; width:25%;">Product</th>
+                                    <th style="text-align:center; width:10%;">Quantity</th>
+                                    <th style="text-align:left; width:30%; padding-left:6.5%;">Time</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td style="vertical-align:middle; text-align:center;">
+                                        <span class="cart-thumb-image">
+                                            015
+                                        </span>
+                                    </td>
+                                    <td style="text-align:center">
+                                        <div class="row-md-6">
+                                            <span>Bún Bò</span>
+                                        </div>
+                                    </td>
+                                    <td style="text-align:center;">
+                                        <div class="row-md-6">
+                                            <span>5</span>
+                                        </div>
+                                    </td>
+                                    <td style="vertical-align:middle; text-align:center;">
+                                        <div class="col-md-6" style="padding:2%">
+                                            08:00
+                                        </div>
+                                        <div class="col-md-6">
+                                            <a class="mu-readmore-btn" style="padding:8%" tabindex="0">Ready</a>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="vertical-align:middle; text-align:center;">
+                                        <span class="cart-thumb-image">
+                                            016
+                                        </span>
+                                    </td>
+                                    <td style="text-align:center">
+                                        <div class="row-md-6">
+                                            <span>Bún Bò</span>
+                                        </div>
+                                        <div class="row-md-6" style="border-top:dotted black 1px">
+                                            <span>Cơm</span>
+                                        </div>
+                                        <div class="row-md-6" style="border-top:dotted black 1px">
+                                            <span>Cháo</span>
+                                        </div>
+                                    </td>
+                                    <td style="text-align:center;">
+                                        <div class="row-md-6">
+                                            <span>1</span>
+                                        </div>
+                                        <div class="row-md-6" style="border-top:dotted black 1px">
+                                            <span>2</span>
+                                        </div>
+                                        <div class="row-md-6" style="border-top:dotted black 1px">
+                                            <span>3</span>
+                                        </div>
+                                    </td>
+                                    <td style="vertical-align:middle; text-align:center;">
+                                        <div class="col-md-6" style="padding:2%">
+                                            08:25
+                                        </div>
+                                        <div class="col-md-6">
+                                            <a class="mu-readmore-btn" style="padding:8%" tabindex="0">Ready</a>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </aside>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
